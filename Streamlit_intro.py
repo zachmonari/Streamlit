60e41fc0247abe859ef2b14026b388f32677ae4d
zach <zachmonari002@gmail.com>
2025-10-13 21:42:52 +0300

    Created a streamlit introduction file

---
+++ Streamlit_intro.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("Hello World!")
+
+movie=st.text_input("What is your favourite movie? ")
+
+click=st.button("Click here")
+
+st.write("## This is a H2 heading")
+
