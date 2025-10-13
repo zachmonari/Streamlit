be88ef9ce2a44b88ccb0238b5cf5cd267b9f10e4
zach <zachmonari002@gmail.com>
2025-10-13 21:52:24 +0300

    Added a button

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.write("Hello World!")
 
 movie=st.text_input("What is your favourite movie? ")
-
 click=st.button("Click here")
+if click:
+    st.write("Your favourite movies is ", movie)
 
 st.write("## This is a H2 heading")
 
