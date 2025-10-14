39fa92acb672c9bac012d5a0ffd95a7189874a89
zach <zachmonari002@gmail.com>
2025-10-14 10:20:12 +0300

    more data visualization

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st
 
 st.title("My Streamlit App")
 st.write("Hello World!:sunglasses:")
@@ -24,18 +24,33 @@ if click:
 
 st.write("## This is a H2 heading")
 
-"""data=pd.read_csv("mnist_test.csv")
-st.write(data)
+df=pd.read_csv("mnist_test.csv")
+st.write(df)
+#st.dataframe(data)
 chart_data=pd.DataFrame(
     np.random.randn(30, 3),
     columns=['red', 'green', 'blue']
 )
 st.bar_chart(chart_data)
 st.line_chart(chart_data)
-"""
+st.area_chart(chart_data)
+st.scatter_chart(chart_data)
+#st.map(chart_data)
+#st.pyplot(chart_data)
+st.altair_chart(chart_data)
+#st.vega_lite_chart(chart_data)
+#st.plotly_chart(chart_data)
+st.bokeh_chart(chart_data)
+st.pydeck_chart(chart_data)
+st.graphviz_chart(chart_data)
+
+#edited=st.data_editor(DataFrame,num_rows="dynamic")
+
 st.text("Dividers")
 st.divider()
-st.help(st.write)
+#st.help(st.write)
 st.html("<p>Foo bar.</p>")
+st.metric("Metrics", 43,5)
+
 
 
