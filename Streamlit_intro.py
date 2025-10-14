0eb15e2035d9aa5fc4fcc3a89a6cc8976002764f
zach <zachmonari002@gmail.com>
2025-10-14 09:50:58 +0300

    more features

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -2,8 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Hello World!")
+st.title("My Streamlit App")
+st.write("Hello World!:sunglasses:")
+#markdown
+st.markdown("My first **App**!")
+st.header("My header")
+st.subheader("My subheader")
+st.badge("My badge")
+st.caption("My caption")
+st.code("My code")
+with st.echo():
+    st.write("My code is this")
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
+st.latex("\int a x^2 \,dx")
 movie=st.text_input("What is your favourite movie? ")
 click=st.button("Click here")
 if click:
@@ -11,7 +24,7 @@ if click:
 
 st.write("## This is a H2 heading")
 
-data=pd.read_csv("mnist_test.csv")
+"""data=pd.read_csv("mnist_test.csv")
 st.write(data)
 chart_data=pd.DataFrame(
     np.random.randn(30, 3),
@@ -19,3 +32,10 @@ chart_data=pd.DataFrame(
 )
 st.bar_chart(chart_data)
 st.line_chart(chart_data)
+"""
+st.text("Dividers")
+st.divider()
+st.help(st.write)
+st.html("<p>Foo bar.</p>")
+
+
