18e69a03d059cd7cd6976ef25c7d75bfc7cbdcc2
zach <zachmonari002@gmail.com>
2025-10-15 12:31:13 +0300

    added download button

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -1,6 +1,7 @@
 #import numpy as np
 #import pandas as pd
 import streamlit as st
+
 st.logo("ZachTechs.jpg")
 st.title("My Streamlit App")
 st.write("Hello World!:sunglasses:")
@@ -23,37 +24,6 @@ if click:
     st.write("Your favourite movies is ", movie)
 
 st.write("## This is a H2 heading")
-"""
-df=pd.read_csv("mnist_test.csv")
-st.write(df)
-#st.dataframe(data)
-chart_data=pd.DataFrame(
-    np.random.randn(30, 3),
-    columns=['red', 'green', 'blue']
-)
-st.bar_chart(chart_data)
-st.line_chart(chart_data)
-st.area_chart(chart_data)
-st.scatter_chart(chart_data)
-#st.map(chart_data)
-#st.pyplot(chart_data)
-st.altair_chart(chart_data)
-#st.vega_lite_chart(chart_data)
-#st.plotly_chart(chart_data)
-st.bokeh_chart(chart_data)
-st.pydeck_chart(chart_data)
-st.graphviz_chart(chart_data)
-
-#edited=st.data_editor(DataFrame,num_rows="dynamic")
-
-st.text("Dividers")
-st.divider()
-#st.help(st.write)
-st.html("<p>Foo bar.</p>")
-st.metric("Metrics", 43,5)
-data=pd.read_csv("mnist_test.csv")
-st.download_button("Download Data",data)
-"""
-
-image=st.image("ZachTechs.jpg")
-st.download_button("Download Image",image)
+with open("ZachTechs.jpg","rb") as file:
+    image = file.read()
+st.download_button("Download Image",image,file_name="ZachTechs.jpg")
