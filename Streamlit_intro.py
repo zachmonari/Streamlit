6b0705cc9ebb11124799a1baf648c1d78279ea8c
zach <zachmonari002@gmail.com>
2025-10-15 12:48:14 +0300

    Added containers and columns

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -1,8 +1,11 @@
 #import numpy as np
 #import pandas as pd
 import streamlit as st
+from PIL import Image
 
-st.logo("ZachTechs.jpg")
+logo = Image.open("ZachTechs.jpg")
+st.image(logo,width=150)
+
 st.title("My Streamlit App")
 st.write("Hello World!:sunglasses:")
 #markdown
@@ -27,3 +30,22 @@ st.write("## This is a H2 heading")
 with open("ZachTechs.jpg","rb") as file:
     image = file.read()
 st.download_button("Download Image",image,file_name="ZachTechs.jpg")
+
+# containers
+c=st.container()
+st.write("This will show last")
+c.write("This will show first")
+c.write("This will show second")
+
+#columns
+col1,col2=st.columns(2)
+col1.write("Column 1")
+col2.write("Column 2")
+
+#modal dialog
+@st.dialog("Sign up")
+def reg_form():
+    name=st.text_input("Enter your name")
+    email=st.text_input("Enter your email")
+
+reg_form()
