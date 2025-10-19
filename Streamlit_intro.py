535848ccf79fbc9b4856e1f9c8995bbaf66feb43
zach <zachmonari002@gmail.com>
2025-10-19 09:22:21 +0300

    Added tiles of ballons

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -138,3 +138,15 @@ with st.container():
     st.bar_chart(np.random.randn(50, 3))
 
 st.write("This is outside the container")
+
+container = st.container(border=True)
+container.write("This is inside the container")
+st.write("This is outside the container")
+
+container.write("This is inside too")
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":balloon:")
