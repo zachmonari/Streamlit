c64fa72436983bbec8c1a9ade5a7ebc099152eff
zach <zachmonari002@gmail.com>
2025-10-22 11:02:59 +0300

    Added a clear button

--- Streamlit_intro.py
+++ Streamlit_intro.py
@@ -223,6 +223,18 @@ if not name:
 st.success("Thank you for inputting a name.")
 
 #cache
+@st.cache_data
+def square(x):
+    return x**2
+
+@st.cache_data
+def cube(x):
+    return x**3
+
+if st.button("Clear All"):
+    # Clear values from *all* all in-memory and on-disk data caches:
+    # i.e. clear values from both square and cube
+    st.cache_data.clear()
 """
 @st.cache_data
 def fetch_and_clean_data(url):
